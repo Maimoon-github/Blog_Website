4bdeba911a627cfccf92f9d84c28b1d5d0502760
Maimoon <maimoon.ideal@gmail.com>
2026-05-31 04:12:56 +0500

    refactor: move page styles and update path aliases to resolve module imports

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { mockPosts, mockCategories } from "../../lib/mockData";
+// import { mockPosts, mockCategories } from "../../lib/mockData";
+import { mockPosts, mockCategories } from "@/lib/mockData";
 import styles from "./page.module.css";
 
 export default function HomePage() {
