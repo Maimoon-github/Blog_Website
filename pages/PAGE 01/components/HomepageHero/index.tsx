88d7f8e9ef56fb3502a134b7e409cb8a9f7ce0cd
Maimoon <maimoon.ideal@gmail.com>
2026-05-30 20:58:53 +0500

    refactor: remove deprecated database migration scripts and unused utility classes

--- pages/PAGE 01/components/HomepageHero/index.tsx
+++
@@ -1,3 +0,0 @@
-export { HomepageHero } from './HomepageHero';
-export type { HomepageHeroProps, HeroContent, StatCalloutData } from './types';
-export { StatCallout } from './subcomponents/StatCallout';
--- pages/PAGE 03/components/FeaturedArticlesGrid/subcomponents/FeaturedArticlesGrid/index.tsx
+++
@@ -1,7 +0,0 @@
-export { FeaturedArticlesGrid } from './FeaturedArticlesGrid';
-export type {
-  FeaturedArticlesGridProps,
-  FeaturedArticle,
-} from './types';
-export { CategoryChip } from './subcomponents/CategoryChip';
-export { ArticleCard } from './subcomponents/ArticleCard';
