82809375a53567cd7882c572280494c0000f058a
Maimoon <maimoon.ideal@gmail.com>
2026-05-31 04:07:24 +0500

    refactor: move home page from (site) route group to root app directory and update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,26 @@
+// // next.config.js
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export', // This enables static export
+//   // Optional: Add a trailing slash to all paths (e.g., /about/ instead of /about)
+//   // trailingSlash: true,
+//   // Optional: Change the output directory name from the default 'out'
+//   // distDir: 'dist',
+// };
+
+// module.exports = nextConfig;
+
+
+
+
+
 // next.config.js
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // This enables static export
-  // Optional: Add a trailing slash to all paths (e.g., /about/ instead of /about)
-  // trailingSlash: true,
-  // Optional: Change the output directory name from the default 'out'
-  // distDir: 'dist',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  allowedDevOrigins: ['192.168.100.12'], // if needed
 };
 
 module.exports = nextConfig;
