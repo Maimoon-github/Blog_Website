146faf2a00c9ba16bdca5440196b90cafea9f579
Maimoon <maimoon.ideal@gmail.com>
2026-05-30 22:15:14 +0500

    feat: initialize frontend project with Next.js and Tailwind CSS configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  // If you use trailing slashes, set: trailingSlash: true,
 };
-
-export default nextConfig;
+module.exports = nextConfig;
